0a81d2943c0daae16031a4d3e566fe93bfbf2999
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2022-06-20 23:31:48 +0530

    Update Snap_app.py

--- Snap_app.py
+++ Snap_app.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 # from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
 from time import sleep
 import streamlit as st
 
